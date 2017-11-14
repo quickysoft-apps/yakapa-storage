43f4d1f335b2dddd70037558e1f255e7fe21b812
neurosoup <neurosoup@gmail.com>
2017-11-14 10:13:13 +0000

    write only if new values

--- src/storage.js
+++ src/storage.js
@@ -3,30 +3,34 @@ import * as path from 'path'
 import * as fs from 'fs'
 import perfy from 'perfy'
 import dataForge from 'data-forge'
-import {
-  lock
-} from 'ki1r0y.lock'
+import equals from 'is-equal-shallow'
+import { lock } from 'ki1r0y.lock'
 
 export default class Storage {
 
   constructor(tag, extractor, value, timestamp, maxDays, maxCount) {
-
-    this._newData = new dataForge.DataFrame([{
-      timestamp: timestamp.slice(0, 19) + '.000Z',
-      value
-    }])
 
     const rootpath = path.join(__dirname, '..', '..', 'storage', tag);
     if (!fs.existsSync(rootpath)) {
       fs.mkdirSync(rootpath)
     }
     this._fullpath = path.join(rootpath, `${extractor}.json`)
-    this._existingData = this.read(maxDays, maxCount)
+    
+    this._newData = new dataForge.DataFrame([{ timestamp: timestamp.slice(0, 19) + '.000Z', value }])
+    this._currentData = this.read(maxDays, maxCount)
+    this._lastData = this._currentData ? this._currentData.tail(1) : null
   }
 
   store(onStored, onError) {
+    
+    const distinct = this._lastData ? this._lastData.except(this._newData, (a, b) => { return equals(a.value, b.value) }) : this._newData    
+    if (distinct.count() === 0) {
+      Common.logger.info('Pas de nouvelles valeurs')
+      return
+    }
+    
     lock(this._filename, (unlock) => {
-      try {
+      try {        
         this.write()
         if (onStored) {
           onStored()
@@ -61,16 +65,16 @@ export default class Storage {
     return false
   }
 
-  write() {
+  write() {    
     let storingData
-    if (this._existingData) {
-      storingData = this._existingData.concat(this._newData)
+    if (this._currentData) {
+      storingData = this._currentData.concat(this._newData)
     } else {
       storingData = this._newData
-    }
+    }    
     perfy.start('writeFile')
     storingData.asJSON().writeFileSync(this._fullpath);
-    Common.logger.info(`${path.basename(this._fullpath)} written in`, perfy.end('writeFile').time, 's')
+    Common.logger.info(`${path.basename(this._fullpath)} written in`, perfy.end('writeFile').time, 's')    
   }
 
 }
